f0eaa7bdb3fa305de87fe902a18fc5e1bf0dd85f
abdulvohid31 <abdulvohidcode@gmail.com>
2024-09-09 16:12:41 +0500

    cdwc

--- src/components/Text/DefText.tsx
+++ src/components/Text/DefText.tsx
@@ -1,6 +1,6 @@
 import { TitleTextColor } from "../Colors"
 
-const DefTitles = ({ text, size }: { text: string, size?: string }) => {
+const DefTitles = ({ text, size }: { text: string | any, size?: string }) => {
     return (
         <h1 className={`${size ? size : 'text-xl'}   font-bold text-transparent bg-clip-text bg-gradient-to-r from-[${TitleTextColor}] to-[#4d7666]`}>
             {text}
